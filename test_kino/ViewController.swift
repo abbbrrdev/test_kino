7f58da4d70641b1a3d35e1104b7e1c5c9c06522b
abbbrrdev <ruanablaikz45@gmail.com>
2025-04-26 01:38:27 +0500

    Initial Commit

---
+++ test_kino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_kino
+//
+//  Created by Рауан Аблайхан on 26.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test_kinoTests/test_kinoTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_kinoTests.swift
+//  test_kinoTests
+//
+//  Created by Рауан Аблайхан on 26.04.2025.
+//
+
+import Testing
+@testable import test_kino
+
+struct test_kinoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
